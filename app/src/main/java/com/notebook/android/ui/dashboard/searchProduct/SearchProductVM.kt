ad17ac9ac98bb2ba05115e16162fd0fc756e1f5f
saw-tarun <tarunk@siliconasiaworks.com>
2021-08-17 14:21:37 +0530

    repeat search fixed.

--- app/src/main/java/com/notebook/android/ui/dashboard/searchProduct/SearchProductVM.kt
+++ app/src/main/java/com/notebook/android/ui/dashboard/searchProduct/SearchProductVM.kt
@@ -1,5 +1,6 @@
 package com.notebook.android.ui.dashboard.searchProduct
 
+import android.os.Looper
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -12,28 +13,34 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class SearchProductVM(
-    val searchRepo:SearchProdRepo
+    val searchRepo: SearchProdRepo
 ) : ViewModel() {
 
-    lateinit var searchProdResponseListener:SearchProdResponseListener
+    lateinit var searchProdResponseListener: SearchProdResponseListener
     fun getUserData() = searchRepo.getUser()
     fun getAllSearchProduct() = searchRepo.getAllSearchProduct()
 
     val email = MutableLiveData<String>()
     // This observer will invoke onEmailChanged() when the user updates the email
-    private val emailObserver = Observer<String> { getSearchProdResult(it) }
+
+    val handler = android.os.Handler(Looper.getMainLooper())
+
+    private val emailObserver = Observer<String> {
+        handler.removeCallbacksAndMessages(null)
+        handler.postDelayed({ getSearchProdResult(it) }, 1000)
+    }
 
     init {
         email.observeForever(emailObserver)
     }
 
-    fun deleteUser(){
+    fun deleteUser() {
         viewModelScope.launch(Dispatchers.IO) {
             searchRepo.deleteUser()
         }
     }
 
-    fun clearCartTableFromDB(){
+    fun clearCartTableFromDB() {
         viewModelScope.launch(Dispatchers.IO) {
             searchRepo.clearCartTable()
             searchRepo.clearAddressTable()
@@ -42,86 +49,87 @@ class SearchProductVM(
         }
     }
 
-    fun addItemsToCart(userID: Int, token: String, prodID: Int?,  prodQty: Int?, updateProd:Int) {
-        Coroutines.main{
+    fun addItemsToCart(userID: Int, token: String, prodID: Int?, prodQty: Int?, updateProd: Int) {
+        Coroutines.main {
             try {
                 searchProdResponseListener.onCartItemAddCallStarted()
-                val prodResponse = searchRepo.addProductToCart(userID, token, prodID, prodQty, updateProd)
+                val prodResponse =
+                    searchRepo.addProductToCart(userID, token, prodID, prodQty, updateProd)
                 prodResponse.let {
-                    if(it.status == 1){
+                    if (it.status == 1) {
                         searchProdResponseListener.onCartItemAdded("Item added successfully ")
                         getCartData(userID, token)
-                    }else if(it.status == 2){
+                    } else if (it.status == 2) {
                         searchProdResponseListener.onInvalidCredential()
-                    }else{
-                        searchProdResponseListener.onFailure(it.msg?:"")
+                    } else {
+                        searchProdResponseListener.onFailure(it.msg ?: "")
                     }
                 }
-            }catch (e: ApiException){
+            } catch (e: ApiException) {
                 searchProdResponseListener.onApiFailure(e.message!!)
-            }catch (e: NoInternetException){
+            } catch (e: NoInternetException) {
                 searchProdResponseListener.onNoInternetAvailable(e.message!!)
             }
         }
     }
 
     fun getCartData(userID: Int, token: String) {
-        Coroutines.main{
+        Coroutines.main {
             try {
                 val prodResponse = searchRepo.getCartData(userID, token)
                 prodResponse.let {
-                    if(it.status == 1){
-                        if(it.cartdata != null){
+                    if (it.status == 1) {
+                        if (it.cartdata != null) {
                             searchRepo.insertCartList(it.cartdata!!)
                         }
-                    }else if(it.status == 2){
+                    } else if (it.status == 2) {
                         searchProdResponseListener.onInvalidCredential()
-                    }else{
-                        searchProdResponseListener.onFailure(it.msg?:"")
+                    } else {
+                        searchProdResponseListener.onFailure(it.msg ?: "")
                     }
                 }
-            }catch (e: ApiException){
+            } catch (e: ApiException) {
                 searchProdResponseListener.onApiFailure(e.message!!)
-            }catch (e: NoInternetException){
+            } catch (e: NoInternetException) {
                 searchProdResponseListener.onNoInternetAvailable(e.message!!)
             }
         }
     }
 
-    fun searchButtonClickListener(view: View){
-        if(email.value?.isNotEmpty() == true){
+    fun searchButtonClickListener(view: View) {
+        if (email.value?.isNotEmpty() == true) {
             getSearchProdResult(email.value!!)
         }
     }
 
-    fun clearSearchTable(){
+    fun clearSearchTable() {
         Coroutines.main {
             searchRepo.clearSearchProductTable()
         }
     }
 
-    fun getSearchProdResult(title:String){
-        if (title.length>1){
-            Coroutines.main{
+    fun getSearchProdResult(title: String) {
+        if (title.length > 1) {
+            Coroutines.main {
                 try {
                     searchProdResponseListener.onApiStarted()
                     searchRepo.clearSearchProductTable()
                     val prodResponse = searchRepo.getSearchProductResult(title)
                     prodResponse.let {
-                        if(it.status == 1){
-                            if(it.product.isEmpty()){
+                        if (it.status == 1) {
+                            if (it.product.isEmpty()) {
                                 searchProdResponseListener.onSuccessListEmpty(it.product)
-                            }else{
+                            } else {
                                 searchRepo.insertAllSearchProduct(it.product)
                                 searchProdResponseListener.onSuccess(it.product)
                             }
-                        }else{
-                            searchProdResponseListener.onFailure(it.msg?:"")
+                        } else {
+                            searchProdResponseListener.onFailure(it.msg ?: "")
                         }
                     }
-                }catch (e: ApiException){
+                } catch (e: ApiException) {
                     searchProdResponseListener.onApiFailure(e.message!!)
-                }catch (e: NoInternetException){
+                } catch (e: NoInternetException) {
                     searchProdResponseListener.onNoInternetAvailable(e.message!!)
                 }
             }
